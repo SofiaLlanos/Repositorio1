55647046276e0a70f078879c649bd26ad1571074
SOFIA LLANOS <sofia.llanos@uc.cl>
2023-04-17 16:39:16 -0400

    cambiar_compu

--- Tarea_2/problema_2/funcion2.f90
+++ Tarea_2/problema_2/funcion2.f90
@@ -6,3 +6,12 @@ subroutine funcion(theta_max, theta,  f_theta)
     f_theta = ((((sin(theta_max/2))**2) - ((sin(theta/2))**2))**(-1.d0/2))/(PI)
     
 end subroutine funcion
+
+subroutine funcion(theta_max, theta,  f_theta)
+    real(8), intent(in) :: theta_max, theta
+    real(8), intent(out) ::  f_theta
+    real(8), parameter :: PI=3.141592653589793d0
+
+    f_theta = 2*((((sin(theta_max/2))**2) - ((sin(theta/2))**2))**(1.d0/2))/(PI)
+    
+end subroutine funcion
